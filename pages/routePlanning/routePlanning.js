ac20548d02a21d03428c72ea61efb1056dde76f8
D <oncwnuIpTUDmQpWn3HCZzHgp2EEQ@git.weixin.qq.com>
2023-01-01 23:44:49 +0800

    添加高德对接api

--- pages/routePlanning/routePlanning.js
+++ pages/routePlanning/routePlanning.js
@@ -1,69 +1,92 @@
 // pages/routePlanning/routePlanning.js
+var amapFile = require('../../libs/amap-wx.130.js');
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    markers: [{
+      id: 0,
+      latitude: 39.989643,
+      longitude: 116.481028,
+      width: 23,
+      height: 33
+    },{
+      id: 0,
+      latitude: 39.90816,
+      longitude: 116.434446,
+      width: 24,
+      height: 34
+    }],
+    distance: '',
+    cost: '',
+    polyline: []
   },
+  onLoad: function() {
+    var that = this;
+    //var key = config.Config.key;
+    var myAmapFun = new amapFile.AMapWX({key: '3b4cf45f703da2660e78287e53d33ccf'});
+    myAmapFun.getDrivingRoute({
+      origin: '116.481028,39.989643',
+      destination: '116.434446,39.90816',
+      success: function(data){
+        var points = [];
+        if(data.paths && data.paths[0] && data.paths[0].steps){
+          var steps = data.paths[0].steps;
+          for(var i = 0; i < steps.length; i++){
+            var poLen = steps[i].polyline.split(';');
+            for(var j = 0;j < poLen.length; j++){
+              points.push({
+                longitude: parseFloat(poLen[j].split(',')[0]),
+                latitude: parseFloat(poLen[j].split(',')[1])
+              })
+            } 
+          }
+        }
+        that.setData({
+          polyline: [{
+            points: points,
+            color: "#0091ff",
+            width: 6
+          }]
+        });
+        if(data.paths[0] && data.paths[0].distance){
+          that.setData({
+            distance: data.paths[0].distance + '米'
+          });
+        }
+        if(data.taxi_cost){
+          that.setData({
+            cost: '打车约' + parseInt(data.taxi_cost) + '元'
+          });
+        }
+          
+      },
+      fail: function(info){
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+      }
+    })
   },
-  handleBack() {
-    console.log('go back');
+  goDetail: function(){
+    wx.navigateTo({
+      url: '../navigation_car_detail/navigation'
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  goToCar: function (e) {
+    wx.redirectTo({
+      url: '../navigation_car/navigation'
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
+  goToBus: function (e) {
+    wx.redirectTo({
+      url: '../navigation_bus/navigation'
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
+  goToRide: function (e) {
+    wx.redirectTo({
+      url: '../navigation_ride/navigation'
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  goToWalk: function (e) {
+    wx.redirectTo({
+      url: '../navigation_walk/navigation'
+    })
   }
 })
