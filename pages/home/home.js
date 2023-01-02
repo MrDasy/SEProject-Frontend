eafa2fa488c0841c896a03c6bc2b9b67fdf22abf
D <oncwnuIpTUDmQpWn3HCZzHgp2EEQ@git.weixin.qq.com>
2023-01-03 01:48:23 +0800

    修改了登录界面

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,72 +1,59 @@
 // pages/home/home.js
+const app = getApp()
 Page({
-
     /**
      * 页面的初始数据
      */
     data: {
-        image: 'https://cdn-we-retail.ym.tencent.com/retail-ui/components-exp/avatar/avatar-v2/1.png',
-        userid: '用户名',
-        userinfo: '信息',
+        userName: '用户名',
+        userAvatar: 'https://cdn-we-retail.ym.tencent.com/retail-ui/components-exp/avatar/avatar-v2/1.png',
+        userInfo: '身份',
+        didLogin: app.globalData.didLogin,
+        loginPmt: "登录测试", 
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
-
+    doLogin: function(c) {
+        wx.request({
+            url: 'http://121.36.225.155:9903/auth/login',
+            method: 'GET',
+            data: {
+                code: c,
+                isTutor: app.globalData.isTutor
+            },
+            success: (res) => {
+                app.globalData.didLogin = true
+                this.setData({
+                    didLogin: true,
+                    loginPmt: "成功",
+                })
+                console.log(res)
+            }
+        })
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
-
+    testLogin: function () {
+        wx.login({
+            success: (res) => {
+                console.log(res);
+                if (res.code) {
+                    this.doLogin(res.code);
+                    console.log(res.code);
+                }
+            }
+        })
     },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
-    },
-
-    testLogin: function() {
-        getApp().doLogin();
+    testProfile: function() {
+        wx.getUserProfile({
+            desc: 'desc',
+            success: res => {
+                console.log(res);
+                this.setData({
+                    userAvatar: res.userInfo.avatarUrl,
+                    userName: res.userInfo.nickName,
+                    userInfo: "身份："+ (app.globalData.isTutor?"家教":"家长"),
+                })
+            },
+        })
     }
 })
