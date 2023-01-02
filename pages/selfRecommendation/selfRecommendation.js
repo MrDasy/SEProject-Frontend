2a47133d2201c1c269e0dd313b12114ea0a45ce7
D <oncwnuIpTUDmQpWn3HCZzHgp2EEQ@git.weixin.qq.com>
2023-01-02 21:03:04 +0800

    修改了缩进样式

--- pages/selfRecommendation/selfRecommendation.js
+++ pages/selfRecommendation/selfRecommendation.js
@@ -1,72 +1,69 @@
 // pages/selfRecommendation/selfRecommendation.js
 Page({
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        isTutor: getApp().globalData.isTutor
+    },
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {},
+    new() {
+        console.log('new');
+        wx.navigateTo({
+            url: '../newSelfRecommendation/newSelfRecommendation'
+        })
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
+    },
 
-  },
-  new() {
-    console.log('new');
-    wx.navigateTo({
-      url: '../newSelfRecommendation/newSelfRecommendation'
-    })
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
+    },
 
-  },
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
+    },
 
-  },
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
+    },
 
-  },
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
+    }
 })
