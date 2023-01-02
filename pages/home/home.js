f2b65a608ac963d791011366805648c783a22d8c
D <oncwnuIpTUDmQpWn3HCZzHgp2EEQ@git.weixin.qq.com>
2023-01-03 03:23:52 +0800

    对接了登录接口

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,5 +1,13 @@
 // pages/home/home.js
 const app = getApp()
+const local_url = "http://localhost:9903"
+const remote_url = "http://121.36.225.155:9903"
+const local_debug = false
+
+function getUrl() {
+    return local_debug ? local_url : remote_url
+}
+
 Page({
     /**
      * 页面的初始数据
@@ -8,25 +16,37 @@ Page({
         userName: '用户名',
         userAvatar: 'https://cdn-we-retail.ym.tencent.com/retail-ui/components-exp/avatar/avatar-v2/1.png',
         userInfo: '身份',
+        loginPmt: "登录",
+
         didLogin: app.globalData.didLogin,
-        loginPmt: "登录测试", 
+
+        userId: 0,
+        sessionKey: "",
+        detailInfo: "",
     },
 
-    doLogin: function(c) {
+    doLogin: function (c) {
         wx.request({
-            url: 'http://121.36.225.155:9903/auth/login',
-            method: 'GET',
+            url: getUrl() + '/auth/session',
+            method: 'POST',
+            header: {
+                'content-type': 'application/x-www-form-urlencoded'
+            },
             data: {
                 code: c,
                 isTutor: app.globalData.isTutor
             },
             success: (res) => {
-                app.globalData.didLogin = true
-                this.setData({
-                    didLogin: true,
-                    loginPmt: "成功",
-                })
                 console.log(res)
+                if (res.data.success) {
+                    app.globalData.didLogin = true
+                    this.setData({
+                        didLogin: true,
+                        loginPmt: "成功",
+                        userInfo: "身份：" + (app.globalData.isTutor ? "家教" : "家长"),
+                        sessionKey: res.data.session_key,
+                    })
+                }
             }
         })
     },
@@ -43,7 +63,7 @@ Page({
         })
     },
 
-    testProfile: function() {
+    testProfile: function () {
         wx.getUserProfile({
             desc: 'desc',
             success: res => {
@@ -51,7 +71,23 @@ Page({
                 this.setData({
                     userAvatar: res.userInfo.avatarUrl,
                     userName: res.userInfo.nickName,
-                    userInfo: "身份："+ (app.globalData.isTutor?"家教":"家长"),
+                })
+                
+                wx.request({
+                    url: getUrl() + '/user/wxinfo',
+                    method: 'POST',
+                    header: {
+                        'content-type': 'application/x-www-form-urlencoded'
+                    },
+                    data: {
+                        userID: this.data.userId,
+                        encryptedData: res.encryptedData,
+                        sessionKey: this.data.sessionKey,
+                        iv:res.iv,
+                    },
+                    success: (res) => {
+                        console.log(res)
+                    }
                 })
             },
         })
